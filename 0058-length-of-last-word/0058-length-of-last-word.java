4acc0f5f6d7bcc5408eea815190af25ed279a45b
prasannalakshmi-02 <prasannalakshmimotati@gmail.com>
2025-10-22 18:45:28 +0530

    Time: 0 ms (100%), Space: 41.8 MB (49%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int i = s.length()-1;
+        while(i>=0 && s.charAt(i) == ' '){
+            i--;
+        }
+        int end = i;
+        while(i >= 0 && s.charAt(i) != ' '){
+            i--;
+        }
+        int start = i;
+        return s.substring(start+1, end+1).length();
+    }
+}
