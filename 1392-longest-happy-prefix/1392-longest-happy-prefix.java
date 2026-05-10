a6ce4e0b687fdd66fb6a45c6ff6cbbc17fa0755f
prasannalakshmi-02 <prasannalakshmimotati@gmail.com>
2026-05-10 08:40:55 +0530

    Time: 15 ms (13.02%), Space: 47.6 MB (59.67%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestPrefix(String s) {
+        int n = s.length();
+        int[] lps = new int[n];
+        for(int i=1; i<n; i++){
+            int j = lps[i-1];
+            while(j>0 && s.charAt(i) != s.charAt(j)){
+                j = lps[j-1];
+            }
+            if(s.charAt(i) == s.charAt(j)){
+                lps[i] = j+1;
+            }
+        }
+        return s.substring(0, lps[n-1]);
+    }
+}
