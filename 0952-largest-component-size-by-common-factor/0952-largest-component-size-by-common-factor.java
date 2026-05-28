e98414329b6027a7d08ad34e75c5e7ff9ddab41a
prasannalakshmi-02 <prasannalakshmimotati@gmail.com>
2026-05-28 06:34:04 +0530

    Time: 67 ms (92.05%), Space: 49.4 MB (76.99%) - LeetHub

--- 0952-largest-component-size-by-common-factor/0952-largest-component-size-by-common-factor.java
+++ 0952-largest-component-size-by-common-factor/0952-largest-component-size-by-common-factor.java
@@ -1,45 +1,66 @@
 class Solution {
-    class DSU {
-        int[] parent;
-        public DSU(int size) {
-            parent = new int[size];
-            for (int i = 0; i < size; i++) {
-                parent[i] = i;
+     public int largestComponentSize(int[] nums) {
+        int n = nums.length;
+        int[] parent = new int[n];
+        int[] size = new int[n];
+        for(int i=0; i<n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++) {
+            int num = nums[i];
+            for(int f=2; f*f<=num; f++) {
+                if(num % f == 0) {
+                    if(map.containsKey(f)) {
+                        union(i, map.get(f), parent, size);
+                    }
+                    else {
+                        map.put(f, i);
+                    }
+                    while(num % f == 0) {
+                        num /= f;
+                    }
+                }
             }
-        }
-        public int find(int x) {
-            if (parent[x] == x) return x;
-            return parent[x] = find(parent[x]); 
-        }
-        public void union(int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-            if (rootX != rootY) {
-                parent[rootX] = rootY;
-            }
-        }
-    }
-    public int largestComponentSize(int[] nums) {
-        int maxVal = 0;
-        for (int num : nums) {
-            maxVal = Math.max(maxVal, num);
-        }
-        DSU dsu = new DSU(maxVal + 1);
-        for (int num : nums) {
-            for (int factor = 2; factor * factor <= num; factor++) {
-                if (num % factor == 0) {
-                    dsu.union(num, factor);         
-                    dsu.union(num, num / factor);    
+            if(num > 1) {
+                if(map.containsKey(num)) {
+                    union(i, map.get(num), parent, size);
+                }
+                else {
+                    map.put(num, i);
                 }
             }
         }
-        int[] componentSizes = new int[maxVal + 1];
-        int maxComponentSize = 0;
-        for (int num : nums) {
-            int root = dsu.find(num);
-            componentSizes[root]++;
-            maxComponentSize = Math.max(maxComponentSize, componentSizes[root]);
+        int max = 0;
+        for(int i=0; i<n; i++) {
+            int p = find(i, parent);
+            max = Math.max(max, size[p]);
         }
-        return maxComponentSize;
+        return max;
+    }
+    public int find(int x, int[] parent) {
+        if(parent[x] == x)
+            return x;
+        return parent[x] = find(parent[x], parent);
+    }
+    public void union(int a, int b,
+                      int[] parent,
+                      int[] size) {
+
+        int pa = find(a, parent);
+        int pb = find(b, parent);
+
+        if(pa == pb)
+            return;
+
+        if(size[pa] < size[pb]) {
+            parent[pa] = pb;
+            size[pb] += size[pa];
+        }
+        else {
+            parent[pb] = pa;
+            size[pa] += size[pb];
+        }
     }
 }
