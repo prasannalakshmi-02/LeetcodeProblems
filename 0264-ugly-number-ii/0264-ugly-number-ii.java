a95e53c9c5e3860a883498df4d315e271e1e5017
prasannalakshmi-02 <prasannalakshmimotati@gmail.com>
2026-08-19 19:12:11 +0530

    Time: 2 ms (99.53%), Space: 43.2 MB (85.64%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+         int[] ugly = new int[n];
+
+    ugly[0] = 1;
+
+    int p2 = 0;
+    int p3 = 0;
+    int p5 = 0;
+
+    for(int i = 1; i < n; i++) {
+
+        int next = Math.min(
+            ugly[p2] * 2,
+            Math.min(ugly[p3] * 3, ugly[p5] * 5)
+        );
+
+        ugly[i] = next;
+
+        if(next == ugly[p2] * 2)
+            p2++;
+
+        if(next == ugly[p3] * 3)
+            p3++;
+
+        if(next == ugly[p5] * 5)
+            p5++;
+    }
+    return ugly[n-1];
+    }    
+}
